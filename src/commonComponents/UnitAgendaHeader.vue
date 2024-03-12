029c5b87726edf74b957f37a6f763c8459285c5b
jiahd <jiahd@dream-it.cn>
2024-03-12 16:28:08 +0800

    领导单位日程init

---
+++ src/commonComponents/UnitAgendaHeader.vue
@@ -0,0 +1,378 @@
+<template>
+  <div class="agenda-header-main">
+    <div class="agenda-header-main-top">
+      <div class="top-tit">
+        <span class="top-tit-icon"><svg-icon icon-class="agenda" /></span>
+        <span>{{ curYearAndMonth }}</span>
+      </div>
+      <div class="top-calendar">
+        <!-- 上移 -->
+        <div class="calendar-arrow arrow-left" @click="calendarToggle('left')">
+          <svg-icon iconClass="rectangle_left"></svg-icon>
+        </div>
+        <div class="calendar-container">
+          <div class="calendar-container-week">
+            <span
+              >{{ weekList[0].monthAndDay }} -
+              {{ weekList[weekList.length - 1].monthAndDay }}</span
+            >
+            <span>(第{{ weeksInYear }}周)</span>
+          </div>
+        </div>
+        <!-- 下移 -->
+        <div class="calendar-arrow arrow-left" @click="calendarToggle('right')">
+          <svg-icon iconClass="rectangle_right"></svg-icon>
+        </div>
+        <span class="calendar-btn" @click="curDayWeekHandle">本周</span>
+      </div>
+      <div class="top-view">
+        <span
+          :class="{ active: timeViewType === 'unit' }"
+          @click="timeViewToggle('unit')"
+          >部门</span
+        >
+        <span
+          :class="{ active: timeViewType === 'person' }"
+          @click="timeViewToggle('person')"
+          >个人</span
+        >
+      </div>
+    </div>
+    <div class="agenda-header-main-operation">
+      <div class="operation-search">
+        <a-input ref="userNameInput" v-model="searchVal" style="width: 206px">
+          <svg-icon slot="suffix" icon-class="search" />
+        </a-input>
+        <span class="operation-btn primary" @click="hanldeSearch">检索</span>
+        <span class="operation-btn" @click="hanldeReset">重置</span>
+      </div>
+      <div class="operation-btns">
+        <span class="operation-btn primary" @click="handleAdd">新增</span>
+        <span class="operation-btn" @click="handleExport">导出</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import moment from "moment";
+export default {
+  name: "AgendaHeader",
+  props: {
+    moduleObject: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    activeDepart: {
+      type: String,
+      default:'',
+    },
+    activeUser: {
+      type: String,
+      default:'',
+    },
+  },
+  data() {
+    return {
+      // 时间视图
+      timeViewType: "unit",
+      // 今天
+      today: "",
+      // 当前日期
+      curDate: "",
+      // 当前月日
+      curMonthAndDay: "",
+      // 当前周
+      curWeek: "",
+      // 当前年月
+      curYearAndMonth: "",
+      // 周开始日期
+      weekStart: "",
+      // 一周数据
+      weekList: [],
+      // 周几中文
+      weekCn: ["一", "二", "三", "四", "五", "六", "日"],
+      // 第几周
+      weeksInYear: "",
+      // 搜索项
+      searchVal: "",
+    };
+  },
+  watch: {},
+  created() {
+    this.initTime(moment());
+    this.sendHeadParams();
+  },
+  methods: {
+    /**
+     * 今天、本周切换
+     */
+    curDayWeekHandle() {
+      this.initTime(moment());
+      this.sendHeadParams();
+    },
+    /**
+     * 头像
+     */
+    getUserPhoto(item) {
+      if (item.photo) {
+        return IDM.url.getWebPath(item.photo);
+      } else {
+        return IDM.url.getModuleAssetsWebPath(
+          require(`../assets/default_${item.sex ? "girl" : "boy"}.png`),
+          this.moduleObject
+        );
+      }
+    },
+    /**
+     * 导出
+     */
+    handleExport() {
+      const params = {
+        startTime: this.weekList[0].date,
+        endTime: this.weekList[this.weekList.length - 1].date,
+        searchText: this.searchVal,
+        deptId:this.activeDepart,
+        userId:this.timeViewType === "person" ? this.activeUser : ""
+      };
+      const url = `ctrl/deptScheduleList/exportWeekData?${IDM.url.stringify(params)}`;
+
+      const a = document.createElement("a");
+      a.style.display = "none";
+      a.setAttribute("target", "_blank");
+      a.href = IDM.url.getWebPath(url);
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    },
+    /**
+     * 新增
+     */
+    handleAdd() {
+      IDM.layer.open({
+        type: 2,
+        title: ["新增日程", "font-size:18px;"],
+        area: ["1200px", "90%"],
+        content: IDM.url.getWebPath(
+          "ctrl/formControl/form?moduleId=180719094152MnF6C2hEPtqIvhjJIxo"
+        ),
+        success: (layero, index) => {
+          top.close = () => {
+            IDM.layer.close(index);
+            this.sendHeadParams();
+          };
+        },
+        end: () => {
+          this.sendHeadParams();
+        },
+      });
+    },
+    /**
+     * 向父组件发送参数
+     */
+    sendHeadParams() {
+      const params = {
+        timeViewType: this.timeViewType,
+        searchVal: this.searchVal,
+      };
+      params.dates = this.weekList.map((item) => item.date).join(",");
+      this.$emit("updateHeadParams", params);
+    },
+    /**
+     * 搜索
+     */
+    hanldeSearch() {
+      this.sendHeadParams();
+    },
+    /**
+     * 重置
+     */
+    hanldeReset() {
+      this.searchVal = "";
+      this.sendHeadParams();
+    },
+    /**
+     * 时间视图切换
+     */
+    timeViewToggle(type) {
+      this.timeViewType = type;
+      this.sendHeadParams();
+    },
+    /**
+     * 初始化时间数据
+     */
+    initTime(today) {
+      // 天数据
+      this.curDate = today.format("YYYY-MM-DD");
+      this.curMonthAndDay = today.format("MM月DD日");
+      this.curWeek = "周" + this.weekCn[today.isoWeekday() - 1];
+      this.curYearAndMonth = today.format("YYYY年MM月");
+      // 周数据
+      const weekStart = today.startOf("isoWeek");
+      this.weekList = [];
+      for (var i = 0; i <= 6; i++) {
+        const day = moment(weekStart).add(i, "days");
+        this.weekList.push({
+          date: day.format("YYYY-MM-DD"),
+          monthAndDay: day.format("MM月DD日"),
+        });
+      }
+      this.weeksInYear = weekStart.isoWeeks();
+      this.weekStart = weekStart.format("YYYY-MM-DD");
+    },
+    /**
+     * 日历切换
+     */
+    calendarToggle(type) {
+      if (type === "left") {
+        this.initTime(
+          moment(this.weekStart, "YYYY-MM-DD").subtract(1, "isoWeek")
+        );
+      } else {
+        this.initTime(
+          moment(this.weekStart, "YYYY-MM-DD").subtract(-1, "isoWeek")
+        );
+      }
+      this.sendHeadParams();
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.agenda-header-main {
+  font-size: 16px;
+  color: #333333;
+
+  .agenda-header-main-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 16px;
+    color: #333333;
+    font-weight: 500;
+    border-bottom: 1px solid rgba(238, 238, 238, 1);
+    user-select: none;
+    height: 50px;
+
+    .top-tit {
+      display: flex;
+      align-items: center;
+      .top-tit-icon {
+        font-size: 28px;
+        color: #0086d9;
+
+        .svg-icon {
+          margin-right: 18px;
+        }
+      }
+    }
+
+    .top-calendar {
+      display: flex;
+      .calendar-arrow {
+        color: #0086d9;
+        font-size: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        margin: 0 16px;
+      }
+      .calendar-container {
+        display: flex;
+        align-items: center;
+        .calendar-container-day,
+        .calendar-container-week {
+          color: #0086d9;
+          font-weight: 400;
+
+          span:nth-child(2) {
+            color: #333;
+            margin-left: 16px;
+          }
+        }
+      }
+
+      .calendar-btn {
+        display: inline-block;
+        cursor: pointer;
+        border: 1px solid rgba(201, 201, 201, 1);
+        border-radius: 2px;
+        line-height: 26px;
+        padding: 0 10px;
+        margin-left: 12px;
+        font-size: 12px;
+        cursor: pointer;
+        // position: absolute;
+        // right: -46px;
+        // top: -2px;
+      }
+    }
+
+    .top-view {
+      span {
+        width: 60px;
+        display: inline-block;
+        border: 1px solid rgba(204, 204, 204, 1);
+        border-radius: 4px;
+        line-height: 30px;
+        text-align: center;
+        cursor: pointer;
+
+        &:first-child {
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+        }
+        &:last-child {
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+          border-left: 0;
+        }
+
+        &.active {
+          background: #0086d9;
+          border-color: #0086d9;
+          color: #fff;
+        }
+      }
+    }
+  }
+
+  .agenda-header-main-operation {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px 0;
+    user-select: none;
+
+    .operation-btn {
+      display: inline-block;
+      cursor: pointer;
+      border: 1px solid rgba(201, 201, 201, 1);
+      border-radius: 2px;
+      line-height: 30px;
+      padding: 0 20px;
+      margin-left: 12px;
+      font-size: 12px;
+      cursor: pointer;
+
+      &.primary {
+        background: #0086d9;
+        border-color: #0086d9;
+        color: #fff;
+      }
+    }
+
+    .operation-setting {
+      font-size: 18px;
+      color: #666666;
+      margin-left: 24px;
+      cursor: pointer;
+      .svg-icon {
+        vertical-align: -0.24em;
+      }
+    }
+  }
+}
+</style>
---
+++ src/mock/IUnitLeaderAgenda.js
@@ -0,0 +1,240 @@
+export const departListMock = [
+  {
+    id: '101',
+    name: '宣传培训处',
+    children: [
+      {
+        name: '张三',
+        relaId: '10101'
+      },
+      {
+        name: '李四',
+        relaId: '10102'
+      },
+      {
+        name: '王五',
+        relaId: '10103'
+      },
+      {
+        name: '赵六',
+        relaId: '10104'
+      }
+    ]
+  },
+  {
+    id: '102',
+    name: '防灾减灾处',
+    children: [
+      {
+        name: '张冠',
+        relaId: '101015'
+      },
+      {
+        name: '李戴',
+        relaId: '10106'
+      }
+    ]
+  },
+  {
+    id: '103',
+    name: '救援协调和预案管理处'
+  },
+  {
+    id: '104',
+    name: '火灾防治管理处'
+  },
+  {
+    id: '105',
+    name: '灾害应对和物资保障处'
+  },
+  {
+    id: '106',
+    name: '危险化学品安全监督管理处'
+  },
+  {
+    id: '107',
+    name: '工贸安全监督管理处'
+  },
+  {
+    id: '108',
+    name: '火灾防治管理处'
+  },
+  {
+    id: '109',
+    name: '灾害应对和物资保障处'
+  },
+  {
+    id: '110',
+    name: '危险化学品安全监督管理处'
+  },
+  {
+    id: '111',
+    name: '工贸安全监督管理处'
+  },
+  {
+    id: '112',
+    name: '火灾防治管理处'
+  },
+  {
+    id: '113',
+    name: '灾害应对和物资保障处'
+  },
+  {
+    id: '114',
+    name: '危险化学品安全监督管理处'
+  },
+  {
+    id: '115',
+    name: '工贸安全监督管理处'
+  }
+]
+
+export const agendaListMock = {
+  "code": "200",
+  "type": "success",
+  "message": "操作成功",
+  "metadata": null,
+  "token": "",
+  "data": [
+    {
+      "date": "2024-03-04",
+      "list": []
+    },
+    {
+      "date": "2024-03-05",
+      "list": [
+        {
+          "porid": "afternoon",
+          "leader": "",
+          "bt": "通知",
+          "agendaType": "1",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "14:30",
+          "id": "240306111544Nc6Tj6tgwAkiPMS1vvK",
+          "endTime": "18:0",
+          "place": "港城广场"
+        },
+        {
+          "porid": "afternoon",
+          "leader": "",
+          "bt": "通知",
+          "agendaType": "2",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "14:30",
+          "id": "240306111645Asd8Jy0Bq50kVNNtUl3",
+          "endTime": "18:0",
+          "place": "港城广场"
+        },
+        {
+          "porid": "afternoon",
+          "leader": "",
+          "bt": "通知",
+          "agendaType": "1",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "14:30",
+          "id": "240306111645K1PG0gJC6BZB527Oldd",
+          "endTime": "18:0",
+          "place": "港城广场"
+        }
+      ]
+    },
+    {
+      "date": "2024-03-06",
+      "list": [
+        {
+          "porid": "morning",
+          "leader": "",
+          "bt": "会议",
+          "agendaType": "1",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "8:30",
+          "id": "240306110154lhzIVs5Db75TOjBNDbw",
+          "endTime": "11:0",
+          "place": "临港"
+        },
+        {
+          "porid": "morning",
+          "leader": "",
+          "bt": "会议",
+          "agendaType": "1",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "8:30",
+          "id": "240306110252HzapnkMBKkMrUDz23Dc",
+          "endTime": "11:0",
+          "place": "临港"
+        },
+        {
+          "porid": "morning",
+          "leader": "",
+          "bt": "会议",
+          "agendaType": "1",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "8:30",
+          "id": "240306110252LSM8O49uRuloFl3qpNU",
+          "endTime": "11:0",
+          "place": "临港"
+        },
+        {
+          "porid": "morning",
+          "leader": "",
+          "bt": "会议",
+          "agendaType": "1",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "8:30",
+          "id": "240306110252XjvxtrGf3pKDPGD6fHs",
+          "endTime": "11:0",
+          "place": "临港"
+        },
+        {
+          "porid": "morning",
+          "leader": "",
+          "bt": "会议",
+          "agendaType": "1",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "8:30",
+          "id": "240306110252ZNYdQCeaTsfRswWF6W2",
+          "endTime": "11:0",
+          "place": "临港"
+        },
+        {
+          "porid": "morning",
+          "leader": "",
+          "bt": "会议",
+          "agendaType": "1",
+          "relateUnit": "",
+          "agendaTypeText": "普通日程",
+          "startTime": "8:30",
+          "id": "240306110252m47R3ZF1bEoz4FNka5d",
+          "endTime": "11:0",
+          "place": "临港"
+        }
+      ]
+    },
+    {
+      "date": "2024-03-07",
+      "list": []
+    },
+    {
+      "date": "2024-03-08",
+      "list": []
+    },
+    {
+      "date": "2024-03-09",
+      "list": []
+    }
+  ],
+  "serverTime": "2024-03-06 12:13:54"
+}
+
+export const userInfoMock = {
+  userOrgId: '101',
+  userid: '10103'
+}
